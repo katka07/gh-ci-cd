e130f2a264477ab152bd87ecb79915a38eebadb9
katka07 <katka07@yahoo.com>
2025-02-07 21:00:21 +0530

    Test Deployment 2

--- src/main/java/com/example/HelloWorldController.java
+++ src/main/java/com/example/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
     
     @GetMapping("/")
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World! This is running on kubernetes POD!";
     }
 }
